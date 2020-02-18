118249eb57ca9128c2b7238e5b3ff06a80101c04
CJ Roberts <cjroberts2013@gmail.com>
2020-02-17 22:55:27 -0600

    Set schema.js to be able to open graphiql when we run the server.js file.

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,11 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const _ = require("lodash");
+
+const users = [
+	{ id: "64", firstName: "JJ", age: 25 },
+	{ id: "84", firstName: "Oakley", age: 26 }
+];
 
 const UserType = new GraphQLObjectType({
 	name: "User",
@@ -8,4 +14,21 @@ const UserType = new GraphQLObjectType({
 		firstName: { type: GraphQLString },
 		age: { type: GraphQLInt }
 	}
+});
+
+const RootQuery = new GraphQLObjectType({
+	name: "RootQueryType",
+	fields: {
+		users: {
+			type: UserType,
+			args: { id: { type: GraphQLString } },
+			resolve(parentValue, args) {
+				return _.find(users, { id: args.id });
+			}
+		}
+	}
+});
+
+module.exports = new GraphQLSchema({
+	query: RootQuery
 });
