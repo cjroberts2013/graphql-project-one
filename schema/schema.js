9fb396dbd14669449e9c1ad51f7e1d0f12482d6b
CJ Roberts <cjroberts2013@gmail.com>
2020-02-17 23:31:30 -0600

    Added json-server to create a quick fake api to test my schema. Then made changes to the schema by removing lodash and my static users, adding axios, and changing the RootQuery resolve to use axios and get data from the fake api

--- schema/schema.js
+++ schema/schema.js
@@ -1,11 +1,6 @@
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
-const _ = require("lodash");
-
-const users = [
-	{ id: "64", firstName: "JJ", age: 25 },
-	{ id: "84", firstName: "Oakley", age: 26 }
-];
+const axios = require("axios");
 
 const UserType = new GraphQLObjectType({
 	name: "User",
@@ -23,7 +18,9 @@ const RootQuery = new GraphQLObjectType({
 			type: UserType,
 			args: { id: { type: GraphQLString } },
 			resolve(parentValue, args) {
-				return _.find(users, { id: args.id });
+				return axios
+					.get(`http://localhost:3000/users/${args.id}`)
+					.then(res => res.data);
 			}
 		}
 	}
